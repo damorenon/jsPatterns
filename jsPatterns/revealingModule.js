37498efd4582bc5f3f0d8585402eb6d61dd7dc68
damorenon <damorenon@gmail.com>
2020-04-14 21:00:54 -0500

    Adding revealing module pattern

---
+++ jsPatterns/revealingModule.js
@@ -0,0 +1,50 @@
+
+//we would simply define all of our functions and variables in the private scope and return an anonymous object 
+//with pointers to the private functionality we wished to reveal as public.
+var myRevealingModule = (function () {
+    var privateVar = "Ben Cherry",
+        publicVar = "Hey there!";
+    function privateFunction() {
+        console.log( "Name:" + privateVar );
+    }
+    function publicSetName( strName ) {
+        privateVar = strName;
+    }
+    function publicGetName() {
+        privateFunction();
+    }
+    // Reveal public pointers to private functions and properties
+    return {
+        setName: publicSetName,
+        greeting: publicVar,
+        getName: publicGetName
+    };
+})();
+myRevealingModule.setName( "Paul Kinlan" );
+
+//-----------------------------------------------------------------------------------
+
+
+// used to reveal private functions and properties with a more specific naming scheme if we would prefer:
+var myRevealingModule = (function () {
+    var privateCounter = 0;
+    function privateFunction() {
+        privateCounter++;
+    }
+    function publicFunction() {
+        publicIncrement();
+    }
+    function publicIncrement() {
+        privateFunction();
+    }
+    function publicGetCount(){
+      return privateCounter;
+    }
+    // Reveal public pointers to private functions and properties
+   return {
+        start: publicFunction,
+        increment: publicIncrement,
+        count: publicGetCount
+    };
+})();
+myRevealingModule.start();
